aedecad15f1a778fd596b471bc8eb3ae8bff95f8
Daniele Giaquinta <104736631+Gia-qui@users.noreply.github.com>
2023-01-14 18:09:41 +0100

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pandas import read_csv
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 from numpy import ravel
 from sklearn.multioutput import MultiOutputClassifier
