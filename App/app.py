3086e9cb0c156baaf5b1d98b1e2aa5cc17c10819
martibb <112756894+martibb@users.noreply.github.com>
2023-01-21 16:56:14 +0100

    Update app.py

--- App/app.py
+++ App/app.py
@@ -4,5 +4,12 @@ from views import views
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
 
+
+@app.after_request
+def add_header(response):
+    response.cache_control.public = True
+    response.cache_control.max_age = 1
+    return response
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=8000, host="127.0.0.1")
